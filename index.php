a5a8030d3c6310b31a5977a28548830f621c554f
Tyler <josephtylerjones@gmail.com>
2014-05-20 06:20:45 -0700

    added initial theme files

---
+++ index.php
@@ -0,0 +1,60 @@
+<?php get_header(); ?>
+
+			<div id="content">
+
+				<div id="inner-content" class="wrap cf">
+
+						<div id="main" class="m-all t-2of3 d-5of7 cf homepage-previews" role="main">
+
+                            <?php query_posts('posts_per_page=10'); ?>
+
+							<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+
+							<article id="post-<?php the_ID(); ?>" <?php post_class( 'cf' ); ?> role="article">
+
+                                <section class="entry-feature cf">
+                                    <?php if (has_post_thumbnail()) : ?>
+                                        <?= the_post_thumbnail('medium') ?>
+                                    <?php else : ?>
+                                    
+                                    <?php endif; ?>
+                                </section>
+
+								<section class="entry-square cf">
+									<h1 class="h2 entry-title"><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h1>
+									<?php the_excerpt(); ?>
+								</section>
+
+							</article>
+
+							<?php endwhile; ?>
+
+									<?php bones_page_navi(); ?>
+
+							<?php else : ?>
+
+									<article id="post-not-found" class="hentry cf">
+											<header class="article-header">
+												<h1><?php _e( 'Oops, Post Not Found!', 'bonestheme' ); ?></h1>
+										</header>
+											<section class="entry-content">
+												<p><?php _e( 'Uh Oh. Something is missing. Try double checking things.', 'bonestheme' ); ?></p>
+										</section>
+										<footer class="article-footer">
+												<p><?php _e( 'This is the error message in the index.php template.', 'bonestheme' ); ?></p>
+										</footer>
+									</article>
+
+							<?php endif; ?>
+
+
+						</div>
+
+					<?php get_sidebar(); ?>
+
+				</div>
+
+			</div>
+
+
+<?php get_footer(); ?>
---
+++ library/js/blog.js
@@ -0,0 +1,61 @@
+(function($){
+
+  $.fn.pin = function(){
+
+    var $this = this,
+          maxWinWidth = 768,
+          pinned = false,
+          $clone,
+          winWidth = $(window).width();
+    
+    $clone = $this.clone();
+    $clone.addClass("pinned").hide();
+    $("BODY").append($clone)
+
+    var pin = function(){
+      $clone.show();
+      pinned = true;
+    }
+
+    var unpin = function(){
+      $clone.hide();
+      pinned = false;
+    }
+      
+    $(window).resize(function(){
+      winWidth = $(window).width();
+      if (winWidth < maxWinWidth){
+        unpin();
+      }
+    });
+
+    $(window).scroll(function(){
+      var top = $this.offset().top - $(window).scrollTop();
+      if (top <= 0 && !pinned && winWidth >= maxWinWidth) {
+        pin();
+      }else if (top > 0 && pinned){
+        unpin();
+      }
+    });
+
+  };
+
+})(jQuery);
+
+jQuery(function(){
+
+  jQuery(".main-nav").pin();
+
+  var icm = parseInt(jQuery(".inner-container").css("margin-top")),
+        header = jQuery(".header"),
+        logo = header.find("#logo");
+      
+  jQuery(window).scroll(function(){
+    var sT = jQuery(window).scrollTop();
+    if (sT < icm) {
+      header.css("top", -1 * sT/2);
+    } 
+  });
+
+});
+
